c262ccd0e187d55b2690969e62dbe21f773d75de
Sun Knudsen <hello@sunknudsen.com>
2022-03-22 08:20:13 -0400

    Saved .auth_private file securely

--- how-to-self-host-hardened-bitcoin-node/tor-client-auth.sh
+++ how-to-self-host-hardened-bitcoin-node/tor-client-auth.sh
@@ -4,17 +4,19 @@
 
 set -e
 
+umask u=rw,go=
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 basedir=$(pwd)
 
 if [ ! -d "$basedir/authorized_clients" ] || [ ! -f "$basedir/hostname" ]; then
-  printf "%s\n" "Run script inside hidden service directory"
+  printf '%s\n' 'Run script inside hidden service directory'
   exit 1
 fi
 
-printf "%s\n" "Enter key pair name and press enter"
+printf '%s\n' 'Enter key pair name and press enter'
 
 read -r name
 
@@ -22,7 +24,7 @@ private_key="$(openssl genpkey -algorithm x25519)"
 
 public=$(echo -n "$private_key" | \
   openssl pkey -pubout | \
-  grep -v " PUBLIC KEY" | \
+  grep -v ' PUBLIC KEY' | \
   base64pem -d | \
   tail --bytes=32 | \
   base32 | \
@@ -30,16 +32,19 @@ public=$(echo -n "$private_key" | \
 
 auth="descriptor:x25519:$(echo -n $public)"
 
-echo $auth > "$basedir/authorized_clients/$name.auth"
+echo $auth | sudo -u debian-tor tee "$basedir/authorized_clients/$name.auth"
+
 
 private=$(echo -n "$private_key" | \
-  grep -v " PRIVATE KEY" | \
+  grep -v ' PRIVATE KEY' | \
   base64pem -d | \
   tail --bytes=32 | \
   base32 | \
   sed 's/=//g')
 
-auth_private="$(cat $basedir/hostname | awk -F  "." '{print $1}'):descriptor:x25519:$private"
+auth_private="$(cat $basedir/hostname | awk -F  '.' '{print $1}'):descriptor:x25519:$private"
+
+echo $auth_private | sudo -u debian-tor tee "$basedir/$name.auth_private"
 
 client_command="$(echo "cat << EOF > ./$name.auth_private\n$auth_private\nEOF\nchmod 600 $name.auth_private")"
 
@@ -47,6 +52,6 @@ printf "%s\n" "Run following on client (within “auth” folder)"
 
 echo "$bold$client_command$normal"
 
-printf "%s $bold%s$normal %s\n" "Don’t forget to run" "systemctl restart tor" "on server"
+printf "%s $bold%s$normal %s\n" 'Don’t forget to run' 'systemctl restart tor' 'on server'
 
-printf "%s\n" "Done"
+printf '%s\n' 'Done'
